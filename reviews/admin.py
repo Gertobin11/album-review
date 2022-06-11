a0c85a050a82769f5dafa08b07c4b7b9dec56abf
Gertobin11 <gertobin11@gmail.com>
2022-06-11 22:01:53 +0100

    Changed the dtae field value in Arist to int as "" was mistakenly added. Added string ethods to the models and did a basic admin site register to view the data entered in the admin panel

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from reviews.models import Artist, Album, Genre, RecordCompany, Review
 
-# Register your models here.
+
+admin.site.register(Artist)
+
+admin.site.register(Album)
+
+admin.site.register(Genre)
+
+admin.site.register(RecordCompany)
+
+admin.site.register(Review)
--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import auth
 
 
 class RecordCompany(models.Model):
@@ -6,17 +7,26 @@ class RecordCompany(models.Model):
     name = models.CharField(max_length=50, help_text='The name of the record company')
     website = models.URLField(help_text='The Companies website')
 
+    def __str__(self):
+        return self.name
+
 
 class Artist(models.Model):
     """ The artists who recorded the album """
-    name = models.CharField(max_length='70', help_text='The name of the recording artist')
+    name = models.CharField(max_length=70, help_text='The name of the recording artist')
     date_formed = models.DateField('The year the artists started')
     website = models.URLField(verbose_name='The artists website')
+
+    def __str__(self):
+        return self.name
 
 
 class Genre(models.Model):
     """ The Genre of the reviewed model """
     name = models.CharField(max_length=50, help_text='The name of the category')
+
+    def __str__(self):
+        return self.name
 
 
 class Album(models.Model):
@@ -28,9 +38,18 @@ class Album(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 
 class Review(models.Model):
     """ The Users Album Review """
     content = models.TextField(help_text='The review text')
     rating = models.IntegerField(help_text='The rating the user has given the album')
-    date_created = models.DateTimeField(auto_now_add=True, help_text='The date and time the review was created')
+    date_created_on = models.DateTimeField(auto_now_add=True, help_text='The date and time the review was created')
+    date_edited_on = models.DateTimeField(null=True, help_text='The date the review was last edited')
+    creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, help_text='The album this review is for')
+
+    def __str__(self):
+        return f'{self.album} reviewed by {self.creator}'
