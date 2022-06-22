9b070496c4c75af43668836ba24f2f9e477e449b
Gertobin11 <gertobin11@gmail.com>
2022-06-23 00:35:26 +0100

    Added options to the reviews admin.py file
    Added display, search, filtering options to the admin models in the reviews app to improve the user experience when viewing the models and make it easier to find the entries required

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,8 +2,34 @@ from django.contrib import admin
 from reviews.models import Artist, Album, Genre, RecordCompany, Review
 
 
-admin.site.register(Artist)
-admin.site.register(Genre)
-admin.site.register(RecordCompany)
-admin.site.register(Review)
-admin.site.register(Album)
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'year_of_release')
+    search_fields = ['title', 'artist__name']
+    ordering = ['title']
+    list_filter = ['genre__name']
+    
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_formed')
+    search_fields = ['name']
+    ordering = ['name']
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    ordering = ['name']
+
+
+@admin.register(RecordCompany)
+class RecordCompanyAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    ordering = ['name']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creator', 'album', 'date_created_on')
+    search_fields = ['title', 'creator__username', 'album__title']
+    ordering = ['-date_created_on']
