fdc0d628c69347a45c16c9d56e3ee6a3d8a44c6f
Gertobin11 <gertobin11@gmail.com>
2022-08-17 08:59:17 +0100

    Added the search form to the drop down nav
    Added a cloned version of the search form to the dropdown menu, refactored the code in the Javascript file for better readability

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,25 @@
 /* Variables */
-const linkDiv = document.getElementsByClassName('links')[0]
-
-const links = linkDiv.children
-
-const linksCopy = linkDiv.cloneNode(true)
-
-const nav = document.getElementsByClassName('main-nav')[0]
-
-nav.appendChild(linksCopy)
-
 const menuBtn = document.getElementsByClassName('menu-btn')[0]
+const linksCopy = document.getElementsByClassName('links')[0].cloneNode(true)
+const nav = document.getElementsByClassName('main-nav')[0]
+const searchCopy = document.getElementsByClassName('search-form')[0].cloneNode(true)
 
-/* Set the status of he burger */
+/* Append the cloned variables to the document and hide them initially */
+nav.appendChild(linksCopy)
+linksCopy.appendChild(searchCopy)
+linksCopy.classList.add('hide')
+
+/* Set the status of the burger */
 let burgerStatus = 'open'
 
-/* Handle the clicking of the burger icon to show the nav and hide it again */
-menuBtn.addEventListener('click', () => {
+const handleBurgerClick = () => {
+    /* Function that opens and close the dropdown menu */
     if (burgerStatus === 'open') {
         menuBtn.classList.add('open')
+        searchCopy.classList.add('search-form-drop')
         linksCopy.classList.remove('links-close')
         linksCopy.classList.add('links-open')
-        linksCopy.classList.add('show')
+        linksCopy.classList.remove('hide')
         burgerStatus = 'closed'
         nav.appendChild(linksCopy)
     }
@@ -32,7 +31,11 @@ menuBtn.addEventListener('click', () => {
         /* Set a timeout to allow the animation to run before hiding the content */
         setTimeout(() => {
             linksCopy.classList.remove('links-open')
-            linksCopy.classList.remove('show')
+            linksCopy.classList.add('hide')
+            searchCopy.classList.remove('search-form-drop')
     }, 500)
     }
-})
+}
+
+/* Handle the clicking of the burger icon to show the nav and hide it again */
+menuBtn.addEventListener('click', handleBurgerClick)
