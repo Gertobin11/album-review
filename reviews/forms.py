c4a272ce5b7b644ab2d7f4ab0a704f8722ef8188
Gertobin11 <gertobin11@gmail.com>
2022-08-17 22:25:23 +0100

    Added stylings to the search form page
     Had an issue with just hiding the label, as I did not want to disable it for accessability, so I created a custom css file and targeted through the child elements of the form to just hide it instead of display: none, also added a placeholder to the form widget

--- reviews/forms.py
+++ reviews/forms.py
@@ -6,3 +6,5 @@ CHOICES = (('album', 'album'), ('reviewer', 'reviewer'), ('group', 'group'))
 class SearchForm(forms.Form):
     search = forms.CharField(max_length=70, min_length=3, required=False)
     search_in = forms.ChoiceField(choices=CHOICES, required=False)
+
+    search.widget.attrs.update({'placeholder': 'Enter Search term'})
