2b31273585b88e6947f398d984124360533fdf1e
Gertobin11 <gertobin11@gmail.com>
2022-08-24 16:33:59 +0100

    Added Auth Functionality
    Added the Authorization functinality with new templates , Added custom stylings to the login page, added more files to gitignore to secure the site

--- album_review/urls.py
+++ album_review/urls.py
@@ -1,9 +1,12 @@
+from argparse import Namespace
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
+    path('accounts', include(('django.contrib.auth.urls', 'album_review'),
+                             namespace='accounts')),
     path('admin/', admin.site.urls),
     path('', include('reviews.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
