0a1d45a7e38d2a5c148bc85749376030d97df8ac
Gertobin11 <gertobin11@gmail.com>
2022-08-28 23:05:02 +0100

    Added responsive Account links with cloned elements in the mobile view

--- static/js/script.js
+++ static/js/script.js
@@ -3,14 +3,30 @@ const menuBtn = document.getElementsByClassName('menu-btn')[0]
 const linksCopy = document.getElementsByClassName('links')[0].cloneNode(true)
 const nav = document.getElementsByClassName('main-nav')[0]
 const searchCopy = document.getElementsByClassName('search-form')[0].cloneNode(true)
+const account = document.getElementById('account')
+const accountLinks = document.getElementById('account-links')
+
+/* Declaring the dropdown Account divs from the cloned node */
+const dropdownAccount = linksCopy.childNodes[7]
+const dropdownAccountLinks = dropdownAccount.childNodes[3]
 
 /* Append the cloned variables to the document and hide them initially */
 nav.appendChild(linksCopy)
 linksCopy.appendChild(searchCopy)
 linksCopy.classList.add('hide')
 
+/* Hide the Account Links initially  on both navs*/
+accountLinks.classList.add('hide')
+console.log(dropdownAccountLinks)
+dropdownAccountLinks.classList.add('hide')
+
+
 /* Set the status of the burger */
 let burgerStatus = 'open'
+
+/* Set the status of the Account nav  */
+let accountStatus = 'closed'
+let dropdownAccountStatus = 'closed'
 
 const handleBurgerClick = () => {
     /* Function that opens and close the dropdown menu */
@@ -36,9 +52,43 @@ const handleBurgerClick = () => {
     }, 500)
     }
 }
+/* Function to handle the click on the account nav item */
+const handleAccountClick = () => {
+    if (accountStatus === 'closed') {
+        accountLinks.classList.remove('hide')
+        accountStatus = 'open'
+    }
+    else if (accountStatus === 'open'){
+        accountStatus = 'closed'
+        accountLinks.classList.add('links-close')
+        /* Set a timeout to allow the animation to run before hiding the content */
+        setTimeout(() => {
+            accountLinks.classList.remove('links-close')
+            accountLinks.classList.add('hide')
+    }, 500)
+    }
+}
+
+/* Function to handle the acount click on smaller screens */
+const handleDropdownAccountClick = () => {
+    if (dropdownAccountStatus === 'closed') {
+        dropdownAccountLinks.classList.remove('hide')
+        dropdownAccountStatus = 'open'
+    }
+    else if (dropdownAccountStatus === 'open'){
+        dropdownAccountLinks.classList.add('hide')
+        dropdownAccountStatus = 'closed'
+    }
+}
 
 /* Handle the clicking of the burger icon to show the nav and hide it again */
 menuBtn.addEventListener('click', handleBurgerClick)
+
+/* Set the Listener on the account element */
+account.addEventListener('click', handleAccountClick)
+
+/* Set the event listener for the dropdown nav o smaller screens */
+dropdownAccount.addEventListener('click', handleDropdownAccountClick)
 
 /* Code to apply animation to messages and auto close the container */
 
