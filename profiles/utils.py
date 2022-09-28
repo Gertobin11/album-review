44ce53f6c69640a9638795672ed5c6c73ad1d9aa
Gertobin11 <gertobin11@gmail.com>
2022-09-28 10:40:31 +0100

    Add Utility function to return days and more data to user profile
    User profile structure has been finalized, remove banner_inmage , replace with friends in profile models

--- profiles/utils.py
+++ profiles/utils.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from reviews.models import Review
 
 
@@ -5,3 +6,9 @@ def number_of_reviews(user):
     """ Function to return the amount of reviews each user has """
     reviews = Review.objects.filter(creator=user)
     return len(reviews)
+
+
+def days_since_joined(joined):
+    """ Return the amount of days since the user joined """
+    today = datetime.now(timezone.utc)
+    return (today - joined).days
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from .models import Profile
-from .utils import number_of_reviews
+from .utils import number_of_reviews, days_since_joined
 from .forms import EditProfileForm
 
 
@@ -24,12 +25,16 @@ def profiles(request):
 def user_profile(request, profile_id):
     """ Return a single user profile """
     profile = get_object_or_404(Profile, pk=profile_id)
+    days_joined = days_since_joined(profile.created_on)
+    time_since_joined = naturaltime(profile.created_on)
     no_of_reviews = number_of_reviews(profile.user)
     profile_form = EditProfileForm(instance=profile)
     context = {
         'profile': profile,
         'no_of_reviews': no_of_reviews,
-        'profile_form': profile_form
+        'profile_form': profile_form,
+        'days_joined': days_joined,
+        'time_since_joined': time_since_joined
     }
     template = 'user_profile.html'
     return render(request, template, context)
