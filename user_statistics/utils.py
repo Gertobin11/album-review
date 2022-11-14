78a69eaa7822bf3320264908440ea9ce7c59cf32
Gertobin11 <gertobin11@gmail.com>
2022-11-14 20:28:09 +0000

    Add graph for reviews per user
    Refactpr the utility function that can return 2 lists depending on the params entered.

--- user_statistics/utils.py
+++ user_statistics/utils.py
@@ -1,16 +1,25 @@
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 from reviews.views import Review, Album
 from django.db.models import Count
 
 
-def get_lists_of_albums_and_no_of_reviews():
+def get_data_lists(type):
     """
         Return 2 lists with the name of each album and
         their corresponding number of reviews
     """
-    no_of_reviews = (Review.objects.all().values(
-                    'album').annotate(review_count=Count('title')))
-    albums = [get_object_or_404(Album, pk=x['album']).title
-              for x in no_of_reviews]
-    reviews = [x['review_count'] for x in no_of_reviews]
-    return albums, reviews
+    if type == 'reviews_per_album':
+        count_of_objects = (Review.objects.all().values(
+                        'album').annotate(count=Count('title')))
+        objects = [get_object_or_404(Album, pk=x['album']).title
+                   for x in count_of_objects]
+
+    elif type == 'reviews_per_user':
+        count_of_objects = (Review.objects.all().values(
+                        'creator').annotate(count=Count('title')))
+        objects = [get_object_or_404(User, pk=x['creator']).username
+                   for x in count_of_objects]
+
+    reviews = [x['count'] for x in count_of_objects]
+    return objects, reviews
--- user_statistics/views.py
+++ user_statistics/views.py
@@ -1,23 +1,41 @@
 from django.shortcuts import render
-from .utils import get_lists_of_albums_and_no_of_reviews
+from .utils import get_data_lists
 from plotly.offline import plot
 import plotly.graph_objs as graphs
 
 
 def statistics(request):
+    """ View to show visualizations of all the data on the website """
 
-    albums, no_of_reviews = get_lists_of_albums_and_no_of_reviews()
-
+    # Get data and implement a chart to displays
+    # the number of reviews per album
+    albums, no_of_reviews = get_data_lists('reviews_per_album')
     figure = graphs.Figure()
     scatter = graphs.Scatter(x=albums, y=no_of_reviews,
                              name='Reviews Per Genre',
                              marker=dict(size=20, color='red'))
     figure.add_trace(scatter)
-    figure.update_layout(xaxis_title="Album", yaxis_title="No. of Reviews")
+    figure.update_layout(xaxis_title="Album", yaxis_title="No. of Reviews",
+                         paper_bgcolor='rgba(0, 0, 0, 0.85)',
+                         font_color='whitesmoke', plot_bgcolor="rgba(0,0,0,1)")
     plot_html = plot(figure, output_type='div')
+
+    # Get data and display a chart of reviews per user
+    users, user_reviews = get_data_lists('reviews_per_user')
+    figure1 = graphs.Figure()
+    scatter1 = graphs.Scatter(x=users, y=user_reviews,
+                              name='Reviews Per Genre',
+                              marker=dict(size=20, color='gold'))
+    figure1.add_trace(scatter1)
+    figure1.update_layout(xaxis_title="Users", yaxis_title="No. of Reviews",
+                          paper_bgcolor='rgba(0, 0, 0, 0.85)',
+                          font_color='whitesmoke',
+                          plot_bgcolor="rgba(0,0,0,1)")
+    plot_html1 = plot(figure1, output_type='div')
 
     template = 'statistics.html'
     context = {
-        'plot_html': plot_html
+        'plot_html': plot_html,
+        'plot_html1': plot_html1
     }
     return render(request, template, context)
