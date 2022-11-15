4cf429b4131f1599653a0a18ef51ec3faa963269
Gertobin11 <gertobin11@gmail.com>
2022-11-15 10:08:16 +0000

    Add 2 additional charts to the statistics page, update utility function
    Add responsiveness queries to make the page viewable on smaller devices

--- user_statistics/utils.py
+++ user_statistics/utils.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
-from reviews.views import Review, Album
-from django.db.models import Count
+from reviews.models import Review, Album, Genre
+from django.db.models import Count, Avg
 
 
 def get_data_lists(type):
@@ -10,15 +10,27 @@ def get_data_lists(type):
         their corresponding number of reviews
     """
     if type == 'reviews_per_album':
-        count_of_objects = (Review.objects.all().values(
-                        'album').annotate(count=Count('title')))
+        count_of_objects = (Review.objects.values(
+                            'album').annotate(count=Count('title')))
         objects = [get_object_or_404(Album, pk=x['album']).title
                    for x in count_of_objects]
 
     elif type == 'reviews_per_user':
-        count_of_objects = (Review.objects.all().values(
-                        'creator').annotate(count=Count('title')))
+        count_of_objects = (Review.objects.values(
+                            'creator').annotate(count=Count('title')))
         objects = [get_object_or_404(User, pk=x['creator']).username
+                   for x in count_of_objects]
+
+    elif type == 'reviews_per_genre':
+        count_of_objects = (Review.objects.values(
+                            'album__genre').annotate(count=Count('title')))
+        objects = [get_object_or_404(Genre, pk=x['album__genre']).name
+                   for x in count_of_objects]
+
+    elif type == 'average_album_rating':
+        count_of_objects = (Review.objects.values(
+                            'album').annotate(count=(Avg('rating'))))
+        objects = [get_object_or_404(Album, pk=x['album']).title
                    for x in count_of_objects]
 
     reviews = [x['count'] for x in count_of_objects]
--- user_statistics/views.py
+++ user_statistics/views.py
@@ -29,7 +29,7 @@ def statistics(request):
     users, user_reviews = get_data_lists('reviews_per_user')
     figure1 = graphs.Figure()
     scatter1 = graphs.Scatter(y=users, x=user_reviews,
-                              name='Reviews Per Genre',
+                              name='Reviews Per User',
                               marker=dict(size=20, color='gold'))
     figure1.add_trace(scatter1)
     figure1.update_layout(yaxis_title="Users", xaxis_title="No. of Reviews",
@@ -43,9 +43,39 @@ def statistics(request):
 
     plot_html1 = plot(figure1, output_type='div')
 
+    # Get data and display a Pie chart for reviews per Genre
+    genre, genre_reviews = get_data_lists('reviews_per_genre')
+    figure2 = graphs.Figure()
+    pie = graphs.Pie(labels=genre, values=genre_reviews,
+                     name='Reviews Per Genre')
+    figure2.add_trace(pie)
+    figure2.update_traces(hoverinfo='label+percent', textinfo='value')
+    figure2.update_layout(paper_bgcolor=('rgba(0,0,0,0.85)'),
+                          font_color='whitesmoke')
+    plot_html2 = plot(figure2, output_type='div')
+
+    # Get data and display chart for average ratings per album
+    album, rating = get_data_lists('average_album_rating')
+    figure3 = graphs.Figure()
+    scatter2 = graphs.Scatter(y=album, x=rating,
+                              name='Average Album Ratings',
+                              mode='markers',
+                              marker=dict(size=15, color='red'))
+    figure3.add_trace(scatter2)
+    figure3.update_layout(yaxis_title="Album", xaxis_title="Average Rating",
+                          paper_bgcolor='rgba(0, 0, 0, 0.85)',
+                          font_color='whitesmoke', plot_bgcolor="rgba(0,0,0,1)")
+
+    # Fade the grid lines to make the grid more distinguishable
+    figure3.update_xaxes(gridcolor='rgba(255, 255, 255, 0.3)')
+    figure3.update_yaxes(gridcolor='rgba(255, 255, 255, 0.3)')
+    plot_html3 = plot(figure3, output_type='div')
+
     template = 'statistics.html'
     context = {
         'plot_html': plot_html,
-        'plot_html1': plot_html1
+        'plot_html1': plot_html1,
+        'plot_html2': plot_html2,
+        'plot_html3': plot_html3
     }
     return render(request, template, context)
