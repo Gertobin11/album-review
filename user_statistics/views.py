6e4c20f0002fada8c519fbb3ea7bb412b2df4c12
Gertobin11 <gertobin11@gmail.com>
2022-11-15 08:38:50 +0000

    Update graph to have albums and users on the y axis for a better UI

--- user_statistics/views.py
+++ user_statistics/views.py
@@ -11,26 +11,36 @@ def statistics(request):
     # the number of reviews per album
     albums, no_of_reviews = get_data_lists('reviews_per_album')
     figure = graphs.Figure()
-    scatter = graphs.Scatter(x=albums, y=no_of_reviews,
+    scatter = graphs.Scatter(y=albums, x=no_of_reviews,
                              name='Reviews Per Genre',
-                             marker=dict(size=20, color='red'))
+                             mode='markers',
+                             marker=dict(size=15, color='red'))
     figure.add_trace(scatter)
-    figure.update_layout(xaxis_title="Album", yaxis_title="No. of Reviews",
+    figure.update_layout(yaxis_title="Album", xaxis_title="No. of Reviews",
                          paper_bgcolor='rgba(0, 0, 0, 0.85)',
                          font_color='whitesmoke', plot_bgcolor="rgba(0,0,0,1)")
+
+    # Fade the grid lines to make the grid more distinguishable
+    figure.update_xaxes(gridcolor='rgba(255, 255, 255, 0.3)')
+    figure.update_yaxes(gridcolor='rgba(255, 255, 255, 0.3)')
     plot_html = plot(figure, output_type='div')
 
     # Get data and display a chart of reviews per user
     users, user_reviews = get_data_lists('reviews_per_user')
     figure1 = graphs.Figure()
-    scatter1 = graphs.Scatter(x=users, y=user_reviews,
+    scatter1 = graphs.Scatter(y=users, x=user_reviews,
                               name='Reviews Per Genre',
                               marker=dict(size=20, color='gold'))
     figure1.add_trace(scatter1)
-    figure1.update_layout(xaxis_title="Users", yaxis_title="No. of Reviews",
+    figure1.update_layout(yaxis_title="Users", xaxis_title="No. of Reviews",
                           paper_bgcolor='rgba(0, 0, 0, 0.85)',
                           font_color='whitesmoke',
                           plot_bgcolor="rgba(0,0,0,1)")
+
+    # Fade the grid lines to make the 2nd graph more distinguishable
+    figure1.update_xaxes(gridcolor='rgba(255, 255, 255, 0.3)')
+    figure1.update_yaxes(gridcolor='rgba(255, 255, 255, 0.3)')
+
     plot_html1 = plot(figure1, output_type='div')
 
     template = 'statistics.html'
