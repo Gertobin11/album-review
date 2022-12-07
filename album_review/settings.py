176b816a0480499c3c87ccd79d5c46ed589a7634
Gertobin11 <gertobin11@gmail.com>
2022-12-07 19:54:54 +0000

    Add cloudinary for media files storage and associated settings

--- album_review/settings.py
+++ album_review/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.sites',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -172,6 +174,11 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR / "static"),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUDIANRY_URL': os.environ.get('CLOUDINARY_URL')
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
