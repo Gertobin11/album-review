9b98813676366622693a7ff743c1743d6990c32b
Gertobin11 <gertobin11@gmail.com>
2022-12-09 09:51:15 +0000

    Remove Nginx media hosting and reapply cloudinary

--- album_review/urls.py
+++ album_review/urls.py
@@ -10,10 +10,8 @@ urlpatterns = [
     path('', include('reviews.urls')),
     path('profiles/', include('profiles.urls')),
     path('statistics', include('user_statistics.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
