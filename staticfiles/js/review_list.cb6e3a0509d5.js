1e4631a2546430d03698779728b5df72e81c6a27
Gertobin11 <gertobin11@gmail.com>
2022-12-13 13:51:41 +0000

    Add coudinary urls for the static images on the website

--- staticfiles/js/review_list.cb6e3a0509d5.js
+++
@@ -1,7 +0,0 @@
-let vinyls = Array.from(document.getElementsByClassName('vinyl'))
-vinyls.forEach((vinyl, idx) => {
-    setTimeout(() => {
-        vinyl.classList.add('fade-in')
-        console.log('hi')
-    }, (idx + 1) * 100)
-})
