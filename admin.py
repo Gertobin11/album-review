a3ef153d9cbce8501cbca577792e66c2853e2e29
Gertobin11 <gertobin11@gmail.com>
2022-12-14 15:27:02 +0000

    Add favicon and add urls to images from cloudinary in the userprofile

--- admin.py
+++ admin.py
@@ -4,4 +4,4 @@ from django.contrib import admin
 class AlbumDiscussionAdminSite(admin.AdminSite):
     site_header = 'Album Discussion Admin Site'
     site_title = 'Album Discussion Admnistration'
-    index_title =  'Album Discussion site admin'
+    index_title = 'Album Discussion site admin'
